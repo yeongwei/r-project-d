56c0dd771fba8439b5426bb7e09447eebf09fc43
YeongWei <yeongwei@mail.com>
2018-11-05 22:03:41 +0800

    Complete implementation with reading from Impala, generate WordR and call REST API

--- render.R
+++ render.R
@@ -1,8 +1,16 @@
 library(rmarkdown)
+library(httr)
 
-pandoc <- Sys.getenv("RSTUDIO_PANDOC")
+# TODO: This dhould be driven by configuration
 Sys.setenv(RSTUDIO_PANDOC="/usr/lib/rstudio-server/bin/pandoc")
 
 render("reportTop20CitiesByTotalOrders.Rmd", 
        quiet = TRUE, 
-       output_dir = "~/git/r-project-d/work")
+       output_dir = "/distribution",
+       output_file = paste("reportTop20CitiesByTotalOrders-", 
+                           format(Sys.time(), "%Y%m%d-%H%M%S"), 
+                           ".docx", 
+                           sep = ""))
+
+status <- content(GET("http://127.0.0.1/distribution/api.php"), "text")
+write(status, "")
